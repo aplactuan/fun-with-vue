515d72e0a93cdfca0157952bb7abef856ecad5e6
aplactuan <aplactuan@gmail.com>
2023-04-10 21:39:03 +0800

    Add component for adding assignment

--- components/AddAssignment.js
+++ components/AddAssignment.js
@@ -1,8 +1,8 @@
 export default {
     template: `
         <form @submit.prevent="add" class="bg-white flex justify-between text-gray-600">
-                <input type="text" placeholder="Enter task" v-model="newAssignment" class="p-1">
-                <button type="submit" class="bg-gray-400 px-3">Add</button>
+            <input type="text" placeholder="Enter task" v-model="newAssignment" class="p-1">
+            <button type="submit" class="bg-gray-400 px-3">Add</button>        
         </form>
     `,
     data() {
